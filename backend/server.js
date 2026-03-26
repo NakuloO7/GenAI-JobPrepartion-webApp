30210ea95419393b7c420ad4a4680d59470dfde9
Nakul <nakulgite08@gmail.com>
2026-03-26 10:11:02 +0530

    ai setup done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const app = require('./src/app');  //in TS we use : import app from './src/app'
 require("dotenv").config();
 
-const connectDb = require('./src/config/database')
+const connectDb = require('./src/config/database');
 connectDb(); // to establish the connect with the database
+
 
 app.listen(3000, ()=>{
     console.log("Server is running on port : 3000");
---
+++ backend/src/services/ai.service.js
@@ -0,0 +1,15 @@
+const {GoogleGenAI} = require('@google/genai');
+
+const ai = new GoogleGenAI({
+    apiKey : process.env.GOOGLE_GENAI_API_KEY
+});
+
+async function invokeGeminAi(){
+    const response = await ai.models.generateContent({
+        model :"gemini-2.5-flash",
+        contents : "Hello Gemini! How are you!"
+    })
+    console.log(response.text);
+}
+
+module.exports = invokeGeminAi;
