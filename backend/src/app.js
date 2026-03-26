ba4f74629702f009c607874a3d86a23ca4e0a290
Nakul <nakulgite08@gmail.com>
2026-03-26 06:34:57 +0530

    auth fe and be integration completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,15 @@
 const express  = require("express");
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cookieParser());
+const corsOptions = {
+    origin :'http://localhost:5173',
+    credentials : true
+}
+app.use(cors(corsOptions));
 
 const authRouter = require('./routes/auth.routes');
 
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, Link} from "react-router";
 import '../auth.form.scss'
+import { useAuth } from "../hooks/useAuth";
 
 const Register = ()=>{
-
+    const {loading, handleRegister}= useAuth();
+    const [username, setUsername] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
     const navigate = useNavigate();
 
-    const handleSubmit = (e)=>{
+
+    const handleSubmit = async(e)=>{
         e.preventDefault();
+        await handleRegister({username, email, password});  //this is the hook that we have made
+        //to keep the ui and business logic seperated
+        navigate('/')
     }
 
     return  (
@@ -18,15 +26,21 @@ const Register = ()=>{
                 <form onSubmit={handleSubmit}>
                     <div className="input-group">
                         <label htmlFor="username">Username</label>
-                        <input type="username" id="username" name="username" placeholder="Enter Username"  />
+                        <input 
+                        onChange={(e)=>setUsername(e.target.value)}
+                        type="username" id="username" name="username" placeholder="Enter Username"  />
                     </div>
                     <div className="input-group">
                         <label htmlFor="Email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Enter email address"  />
+                        <input 
+                        onChange={(e)=>setEmail(e.target.value)}
+                        type="email" id="email" name="email" placeholder="Enter email address"  />
                     </div>
                     <div className="input-group">
                         <label htmlFor="Email">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Enter password here"  />
+                        <input 
+                        onChange={(e)=>setPassword(e.target.value)}
+                        type="password" id="password" name="password" placeholder="Enter password here"  />
                     </div>
                     <button className="button primary-button">
                         Register
